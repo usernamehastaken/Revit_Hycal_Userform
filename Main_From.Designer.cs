43988e264ceb097c64996f89d9a806147945cb64
xx c <cxx_pb@126.com>
2021-06-24 09:44:18 +0800

    init

---
+++ Main_From.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Revit_Hycal_Userform
+{
+    partial class Main_From
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.File_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.New_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Save_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Open_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Edit_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Cal_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DatabaseView_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File_ToolStripMenuItem,
+            this.Edit_ToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 25);
+            this.menu.TabIndex = 0;
+            // 
+            // File_ToolStripMenuItem
+            // 
+            this.File_ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.New_ToolStripMenuItem,
+            this.Save_ToolStripMenuItem,
+            this.Open_ToolStripMenuItem,
+            this.Exit_ToolStripMenuItem});
+            this.File_ToolStripMenuItem.Name = "File_ToolStripMenuItem";
+            this.File_ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.File_ToolStripMenuItem.Text = "文件";
+            // 
+            // New_ToolStripMenuItem
+            // 
+            this.New_ToolStripMenuItem.Name = "New_ToolStripMenuItem";
+            this.New_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.New_ToolStripMenuItem.Text = "新建工程";
+            // 
+            // Save_ToolStripMenuItem
+            // 
+            this.Save_ToolStripMenuItem.Name = "Save_ToolStripMenuItem";
+            this.Save_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Save_ToolStripMenuItem.Text = "保存工程";
+            // 
+            // Open_ToolStripMenuItem
+            // 
+            this.Open_ToolStripMenuItem.Name = "Open_ToolStripMenuItem";
+            this.Open_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Open_ToolStripMenuItem.Text = "打开工程";
+            // 
+            // Exit_ToolStripMenuItem
+            // 
+            this.Exit_ToolStripMenuItem.Name = "Exit_ToolStripMenuItem";
+            this.Exit_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Exit_ToolStripMenuItem.Text = "退出程序";
+            // 
+            // Edit_ToolStripMenuItem
+            // 
+            this.Edit_ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Cal_ToolStripMenuItem,
+            this.DatabaseView_ToolStripMenuItem});
+            this.Edit_ToolStripMenuItem.Name = "Edit_ToolStripMenuItem";
+            this.Edit_ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.Edit_ToolStripMenuItem.Text = "编辑";
+            // 
+            // Cal_ToolStripMenuItem
+            // 
+            this.Cal_ToolStripMenuItem.Name = "Cal_ToolStripMenuItem";
+            this.Cal_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.Cal_ToolStripMenuItem.Text = "局部阻力系数分析";
+            // 
+            // DatabaseView_ToolStripMenuItem
+            // 
+            this.DatabaseView_ToolStripMenuItem.Name = "DatabaseView_ToolStripMenuItem";
+            this.DatabaseView_ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.DatabaseView_ToolStripMenuItem.Text = "数据库查看";
+            // 
+            // Main_From
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Main_From";
+            this.ShowIcon = false;
+            this.Text = "风管水力计算及优化";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem File_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem New_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Save_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Open_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Exit_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Edit_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Cal_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DatabaseView_ToolStripMenuItem;
+    }
+}
+
